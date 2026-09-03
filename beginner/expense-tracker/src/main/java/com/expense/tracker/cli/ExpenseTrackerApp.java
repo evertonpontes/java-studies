27e22d60c5b9f5881912f273e08eca886c1c5fdd
Everton Pontes Pereira <evertonpontesp@gmail.com>
2026-09-03 10:54:55 -0300

    (feat) add expense-tracker initial config

---
+++ beginner/expense-tracker/src/main/java/com/expense/tracker/cli/ExpenseTrackerApp.java
@@ -0,0 +1,10 @@
+package main.java.com.expense.tracker.cli;
+
+public class ExpenseTrackerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world from expense tracker app.");
+
+	}
+
+}
